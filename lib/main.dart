55c18827ec0ff038d2fae4723703a3581025c359
Pedro <pedrobraganick@hotmail.com>
2019-12-16 13:16:54 -0200

    commit inicial

---
+++ lib/main.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+import 'package:lojavirtual/screens/home_screen.dart';
+
+void main() => runApp(HomePage());
+
+class HomePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Clothing',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+        primaryColor: Color.fromARGB(255, 4, 125, 141)
+      ),
+      debugShowCheckedModeBanner: false,
+      home: HomeScreen(),
+    );
+  }
+}
